5d8ca1e6d17167ff3ee0b18399c117d9cbdb9dc5
luisiiin <luisruv.03@gmail.com>
2026-02-13 10:13:54 -0700

    Añadir clases

---
+++ JavaApplication2/src/Vehiculo.java
@@ -0,0 +1,26 @@
+public class Vehiculo {
+    private String marca;
+    private double precio;
+    private String motor;
+    
+    public Vehiculo (String marca, double precio, String motor){
+        this.marca = marca;
+        this.precio = precio;
+        this.motor = motor;
+        
+    }
+    public String getMarca(){
+        return this.marca;
+    }
+    
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    
+    @Override
+    public  String toString(){
+        return "Marca: " +this.marca +"/n"+
+                "Precio: " +this.precio + "/n"+
+                 "Motor: " + this.motor +"/n";
+    }
+}
